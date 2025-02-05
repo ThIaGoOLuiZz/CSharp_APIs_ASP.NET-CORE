42f092190f912982a1880224ce8fa2db35ead179
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-02-05 20:51:53 -0300

    Adicionado nova migration para popular tabela Produtos

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c900e0db4b5d377c7b0add53e2d260aa6e67eccd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3deb8cca81041f5325a69e2a2ec8e540b72837f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
