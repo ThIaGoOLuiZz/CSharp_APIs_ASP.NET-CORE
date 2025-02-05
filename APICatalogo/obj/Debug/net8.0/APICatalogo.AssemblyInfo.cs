c900e0db4b5d377c7b0add53e2d260aa6e67eccd
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-02-05 20:32:29 -0300

    Aplicado Data Annotations nas colunas

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42983f6777760e843be890bb96abfa01e1d76f47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70d60684684d7e422121479352ec9742fc259ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
