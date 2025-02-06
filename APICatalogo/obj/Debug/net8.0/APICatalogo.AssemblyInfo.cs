e185682dca02dc8076b666033b6513f629ca79f7
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-02-05 23:22:09 -0300

    Adicionado nova rota Post para criar um produto

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a57cb4413e8186d752ae951e8c0fe7e8cf2eac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13f418292fe987b27b8473471c4f0e7796499bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
