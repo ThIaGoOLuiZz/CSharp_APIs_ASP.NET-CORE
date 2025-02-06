1b818071a02f4bb0896e292fdd7ad6b0b9549330
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-02-05 23:29:24 -0300

    Adicionado rota Put para atualizar os dados de um produto

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13f418292fe987b27b8473471c4f0e7796499bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e185682dca02dc8076b666033b6513f629ca79f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
