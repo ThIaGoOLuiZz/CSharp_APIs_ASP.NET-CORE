f44dca01e256140f84d93a29d8e5c23eae7cfb1c
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-02-06 22:02:37 -0300

    Adicionado metodo GET para buscar todas as Categorias

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,32 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = _context.Categorias.ToList();
+
+            if(categorias is null)
+            {
+                return NotFound("Categorias não encontradas...");
+            }
+
+            return categorias;
+        }
+    }
+}
--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b818071a02f4bb0896e292fdd7ad6b0b9549330")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2671e0a49029b7e380b7dd48611af9f22f89c0c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
